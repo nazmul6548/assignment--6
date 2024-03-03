29b6a3bd1cc2849f80abf4c60eb8cb2078d208ec
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-03 10:26:32 +0600

    task complete

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ info.forEach(element => {
     <div class="flex flex-col lg:flex-row mb-5">
        <!--  -->
        <div class="flex">
-        
-       
        <div class="indicator mt-8 ml-8">
     <span class="indicator-item badge ${element.isActive ? 'bg-green-500' :
     'bg-red-500'} text-white px-2 py-1 rounded-full">
@@ -30,14 +28,11 @@ info.forEach(element => {
     </div>
     </div>
 
-
-
-
         <div>
             <div class=" mt-10">
                 <div class="flex text-left w-3/4 m-auto">
-                    <p class="">${element.category}</p>
-                    <p class="ml-14">${element.author.name}</p>
+                    <p class="">#${element.category}</p>
+                    <p class="ml-14">Author : ${element.author.name}</p>
                       
                   </div>
                   <h4 class="text-center lg:text-left text-3xl pt-5 pb-5 w-full  lg:w-3/4 m-auto">${element.title}</h4>
@@ -57,7 +52,7 @@ info.forEach(element => {
                         <img src="./icon/Group 18.png" alt="">
                         <p class="ml-4">${element.posted_time}</p>
                     </div>
-                    <img src="./icon/Group 40106.png" alt="">
+                    <button onclick="handleButtonClick('${element.title}',${element.view_count})"><img src="./icon/Group 40106.png" alt=""> </button>
                   </div>
               </div>
               
@@ -70,4 +65,32 @@ info.forEach(element => {
     cardContainer.appendChild(cardInfo)
 });
 }
+let count =0;
+const handleButtonClick= (title,view) => {
+//     const titleStore = title
+//     // console.log(titleStore);
+//     const viewStore = view;
+// console.log(viewStore);
+// const addTitle = document.getElementById('title');
+// addTitle.innerText=titleStore;
+// const addView = document.getElementById('view');
+// addView.innerText=viewStore;
+const store = document.getElementById('storeId');
+const div = document.createElement('div');
+div.style.display='flex';
+div.style.justifyContent="space-between";
+div.style.backgroundColor="#FFFFFF";
+div.style.padding="10px";
+div.style.borderRadius='10px';
+div.style.marginTop='10px';
+const p = document.createElement('p');
+p.innerText=title;
+const p2 = document.createElement('p');
+p2.innerText=view;
+div.appendChild(p);
+div.appendChild(p2);
+store.appendChild(div);
+}
+
+
 callMyApi();
